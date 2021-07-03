0fffe71cf72e695f850dd50776022c0da13263d3
Vincent <vao2002@gmail.com>
2021-07-03 16:18:58 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+const r = document.getElementById('main');
+r.remove();
+
+const element = document.createElement('h1');
+const sweetMessage = document.createElement('li');
+
+element.id="victory";
+sweetMessage.innerHTML = "Vincent is the champion";
+
+document.body.appendChild(element);
+element.appendChild(sweetMessage);
+
+const newHeader = document.querySelector('h1#victory');
